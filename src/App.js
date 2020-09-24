45a5c21815948135e44ff33f2291e857eeabd8ef
Kengston <danildc@mail.ru>
2020-09-24 21:07:26 +0300

    Weather API added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import React from 'react';
+import Axios from "axios";
 import './App.css';
 
 class App extends React.Component {
 
-  //state
-  state = {
-    coords:{
-      latitude: 59,
-      longitude: 90
-    }
-  }
-
-  componentDidMount() {
-    //get device location
-    if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition((position) => {
-        let newCoords = {
-          latitude:position.coords.latitude,
-          longitude:position.coords.longitude
+    //state
+    state = {
+        coords: {
+            latitude: 59,
+            longitude: 90
         }
-
-        this.setState({coords:newCoords});
-
-      })
-    } else {
-      console.log("Pizdec")
     }
-  }
 
-  render() {
-    return (
-        <div className="App">
+    componentDidMount() {
+        //get device location
+        if (navigator.geolocation) {
 
-        </div>
-    );
-  }
+            navigator.geolocation.getCurrentPosition((position) => {
+                let newCoords = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                }
+
+                this.setState({coords: newCoords});
+
+                //Api call
+                Axios.get(`http://api.weatherstack.com/current?access_key=cc23925749e752253199ee5e54e95751&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
+                    console.log(res)
+                })
+
+            })
+        } else {
+            console.log("Pizdec")
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
 }
 
 
