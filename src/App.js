d99c92c4a4dd9ae6721c5b7ddb555ff0a6063956
Kengston <danildc@mail.ru>
2020-09-24 21:27:04 +0300

    weatherData

--- src/App.js
+++ src/App.js
@@ -26,9 +26,19 @@ class App extends React.Component {
 
                 //Api call
                 Axios.get(`http://api.weatherstack.com/current?access_key=cc23925749e752253199ee5e54e95751&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
-                    console.log(res)
+                    let weatherData = {
+                        location: res.data.location.name,
+                        temperature: res.data.current.temperature,
+                        description: res.data.current.weather_descriptions[0],
+                        region: res.data.location.region,
+                        country: res.data.location.country,
+                        wind_speed: res.data.current.wind_speed,
+                        pressure: res.data.current.pressure,
+                        precip: res.data.current.precip,
+                        humidity: res.data.current.humidity,
+                        img: res.data.current.weather_icons
+                    }
                 })
-
             })
         } else {
             console.log("Pizdec")
